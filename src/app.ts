9c427f3508c4a1bd8d4b22139d0c01c80197a04d
Olivier <30660657+olivierstl@users.noreply.github.com>
2022-12-03 17:07:00 +0100

    fix: out first ts error fixed

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ let i = 0
 
 const increment = (e) => {
   i++
-  counter.querySelector('span').innerText = i
+  counter.querySelector('span').innerText = i.toString()
 }
 
 counter.addEventListener('click', increment)
